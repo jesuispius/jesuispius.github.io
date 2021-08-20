5a030c6b4e760da2ae676eb3f5dd056082a0c3de
jesuispius <phuocpius.nguyen8@gmail.com>
2021-08-20 14:54:59 -0700

    Optimize the image, clean unused code, improve the accessibility, and SEO

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,6 +1,6 @@
 
 
-let intro_main_location = document.getElementById("intro-section-main");
+let intro_main_location = document.getElementById("intro-section-main-content");
 
 function backToTop() {
     document.body.scrollTop = 0;
@@ -17,6 +17,7 @@ function createBackToTopButton() {
     btn.style.right = "20px";
     btn.style.display = "none";
     btn.style.borderRadius = "50%";
+    btn.ariaLabel = "back-to-top";
     btn.innerHTML += `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
--- js/resume.js
+++ js/resume.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     // Render the skills items //
     const skill_items = [
         "C++", "Python", "Java", "JavaScript", 
@@ -9,16 +8,15 @@ $(document).ready(function () {
 
     let render_item = $("<div></div>").addClass("resume-content-skills-items");
     skill_items.map(item => {
-        let item_badge = $("<div></div>")
-                            .addClass("badge text-dark mt-3")
-                            .css("background-color", "#FFD54F")
-                            .text(item);
-        render_item.append(item_badge);
+        render_item.append(
+            $("<div></div>")
+                .addClass("badge text-dark mt-3")
+                .css("background-color", "#FFD54F")
+                .text(item)
+        );
     });
     $(".resume-content-skills").append(render_item);
 
-
-    
     // Render the languages items //
     const languages_items = [
         {lang: "Vietnamese", level_text: "Native or bilingual proficiency", level_percent: "100"},
@@ -28,15 +26,20 @@ $(document).ready(function () {
 
     let render_lang_item = $("<div></div>");
     languages_items.map(item => {
-        let lang_text = $("<div></div>").text(item.lang).addClass("lang-text");
-        let lang_progress = $("<div></div>")
-                            .addClass("progress progress-bar-striped bg-warning mt-2 mb-2")
-                            .css("width", item.level_percent.concat("%"))
-                            .attr("aria-valuenow", item.level_percent)
-                            .attr("aria-valuemin", "0")
-                            .attr("aria-valuemax", "100")
-                            .attr("role", "progressbar");
-        render_lang_item.append(lang_text, lang_progress);
+        render_lang_item.append(
+            $("<div></div>").text(item.lang).addClass("lang-text"),
+            $("<div></div>").addClass("progress mt-2 mb-2").append(
+                $("<div></div>")
+                    .addClass("progress-bar progress-bar-striped bg-warning text-dark fw-bold")
+                    .css("width", item.level_percent.concat("%"))
+                    .attr("aria-valuenow", item.level_percent)
+                    .attr("aria-valuemin", "0")
+                    .attr("aria-valuemax", "100")
+                    .attr("role", "progressbar")
+                    .attr("aria-label", item.lang)
+                    .text(item.level_percent.concat("%"))
+            )
+        );
     });
     $(".resume-content-languages").append(render_lang_item);
 });
